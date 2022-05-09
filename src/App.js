c52f0713cbf25ae52fe08172ccddeec6be10bfba
arvinthinnive <amanivannaboopathy@inniveinc.com>
2022-05-09 09:43:04 +0530

    states example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import Home from "./Home";
 import "./styles.css";
 
 export default function App() {
-  var name = "Sharmila"
+  
+  const [tempValue, setTempValue] = useState("sharm");
+
   return (
     //JSX == JAVASCRIPT XML
     <div className="App">
-      <h1>{name}</h1>
+      <h1>{tempValue}</h1>
+      <button onClick={function(){
+        setTempValue("changed")
+      }} > Click to change the state</button>
       <h2>Start editing to see some magic happen!</h2>
       <Home/>
     </div>
