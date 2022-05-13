33225569f51d123c382a91fd376703aa49603793
arvinthinnive <amanivannaboopathy@inniveinc.com>
2022-05-13 09:31:39 +0530

    conditional rendering re merged

--- src/App.js
+++ src/App.js
@@ -1,74 +1,33 @@
 import { useState } from "react";
 import Home from "./Home";
 import "./styles.css";
-let data  = [
-  {
-      name : "Sharmila",
-      experience : 5,
-      technologyKnown : ["node", "javascript", "html", "css"],
-      place : "chennai",
-      company : 'TCS'
-  },
-  {
-      name : "Nithesh",
-      experience : 4,
-      technologyKnown : ["javascript", "bootstrap"],
-      place : "tanjore",
-      company : 'TCS'
-  },
-  {
-      name : "Arvinth",
-      experience : 3,
-      technologyKnown : ["javascript","node", "firebase"],
-      place : "vilupuram",
-      company : 'MP'
-  },
-  {
-      name : "Sivakumar",
-      experience : 0.2,
-      technologyKnown : ["javascript"],
-      place : "chennai",
-      company : 'HCL'
-  },
-  {
-      name : "Saravana Kumar",
-      experience : 10,
-      technologyKnown : ["node", "firebase", "javascript", "bootstrap", "oAuth", "jQuery", "React", "Angular"],
-      place : "chennai",
-      company : 'Mind Tree'
-  }
-];
+
 export default function App() {
   
-
-  const [inputs , setInputs] = useState({});
-  const [dataCopy, setDataCopy] =useState(data);
-
-
-
-
-
-function handleDelete(){
-console.log(inputs);
-setDataCopy(()=>{
-  return data.filter(e=> !Object.keys(inputs).includes(e.name));
-})
-}
-
+  const [name, setName] = useState("");
+  const [pass, setPass] =useState("");
 
   return (
-    
-<div>
-    {dataCopy.map(e=>(
-      <>
-      <input key={e.name} type="checkbox" onChange={()=>{setInputs(previousState =>{ return {...previousState, [e.name] : "checked"} })}}></input>
-      {e.name}
+    //JSX == JAVASCRIPT XML
+    <div className="App">
+      Name <input onChange={function(e){
+        debugger
+        setName(e.target.value)
+      }} type="text"></input>
+      {/* {name} */}
+      Password <input type="text" onChange={function(e){
+        debugger
+        setPass(e.target.value)
+      }}></input>
+      {/* {pass} */}
       <br></br>
-      </>
-    ))}
+      <button >Enter</button>
 
 
-    <button onClick={()=>{handleDelete()}}>Delete</button>
-</div>
+      {name == "Sharmila" && pass =="sha" ? (<div>Login is successful</div>): (<div>Something is wrong</div>)}
+
+
+    </div>
+
   );
 }
